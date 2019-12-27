680d5d7601bc69212d3fa751fc4503aa9ad1d55f
Jiang Ke <726325604@qq.com>
2019-12-27 10:14:09 +0800

    WIQEUIQOHFH

---
+++ commomalityCSS.js
@@ -0,0 +1,4 @@
+document.writeln('<link rel="stylesheet" href=".\\style\\heade-base.css">');
+document.writeln('<link rel="stylesheet" href=".\\bootstrap-3.3.7-dist\\css\\bootstrap.min.css">');
+document.writeln('<link rel="shortcut icon" href=".\\images\\favicon.ico" type="image/x-icon"/>');
+document.writeln('<link rel="stylesheet" href=".\\images\\iconfont\\font_1487086_dlbde3jet9t\\iconfont.css"></link>');
